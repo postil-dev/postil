ae791e3816be7f900b0ad9efb219081f04537d62
Mörgæsis <morgaesis+git@morgaes.is>
2026-05-22 02:18:07 +0000

    fix: keep crawler files static

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/install", "/login"],
     },
     sitemap: "https://postil.dev/sitemap.xml",
   };
