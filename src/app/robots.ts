22b8b4e24c1b1182a093f7a819b1e695563516e6
Mörgæsis <morgaesis+git@morgaes.is>
2026-05-22 02:12:33 +0000

    feat: add crawler guidance files

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: ["/install", "/login"],
     },
     sitemap: "https://postil.dev/sitemap.xml",
   };
