88024ad9c54fcbef352ebab84e45fa0053c5fb85
Mörgæsis <morgaesis+git@morgaes.is>
2026-05-22 04:34:57 +0000

    fix: serve crawler guidance from app route

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,29 @@
 import type { MetadataRoute } from "next";
 
+const crawlableBots = [
+  "GPTBot",
+  "ClaudeBot",
+  "Google-Extended",
+  "PerplexityBot",
+  "CCBot",
+];
+
+const rules = [
+  {
+    userAgent: "*",
+    allow: "/",
+    disallow: ["/install", "/login"],
+  },
+  ...crawlableBots.map((userAgent) => ({
+    userAgent,
+    allow: "/",
+    disallow: ["/install", "/login"],
+  })),
+];
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules,
     sitemap: "https://postil.dev/sitemap.xml",
   };
 }
